96b1abecc54df6325fb3b5c1e7884df527815d0d
johannkrugell <johannkrugell@gmail.com>
2022-09-22 07:13:14 +1000

    ADD REDUCE METHOD TO CREATE HASH FROM STRING Identifies the substring based on the length on the word and matches any charaters. The matches are add to a substring array and then reduced to a hash
    On branch substring-match
    Changes to be committed:
    modified:   substrings.rb

--- substrings.rb
+++ substrings.rb
@@ -1,22 +1,36 @@
 # declare dictionary of words
 phrase = ["below","down","go","going","horn","how","howdy","it","i","low","own",
           "part","partner","sit"]
-
+          
 def substrings(word, dictionary)
+  # array of substrings found
   substring_found = []
-  dictionary.each do |string|
-    if string.include? word 
-      
-      substring_found << string
-      substring_found
-
+  
+  # if phrase is provided split into words
+  phrase = word.split(/\W+/)
+  
+  # for each word in phrase check if a character matches within the length range
+  # of the word
+  phrase.each do |word|
+    dictionary.each do |string|
+      if string.match?(/[#{word}]{#{word.length < 3? word.length : 3},#{word.length}}/i)    
+        substring_found << string
+      end
     end
   end
+  
+  # declare empty hash
+  hash_listing = {}
+
+  # reduce substring found into hash
   substring_found.reduce(Hash.new(0)) do |substring, count|
     substring[count]+=1  
-    p substring
+    hash_listing = substring
   end
-  
+
+  # print hash
+  p hash_listing
 end
 
-substrings("go",phrase)
+substrings("Below",phrase)
+
